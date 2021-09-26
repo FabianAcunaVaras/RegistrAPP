7801fcaa56ee1f2307926688ea54a83d4b2f9e31
Balf <fabalf2@gmail.com>
2021-09-25 22:22:49 -0300

    asd
    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: ':NombreUsuario',
     loadChildren: () => import('./principal/principal.module').then( m => m.PrincipalPageModule)
+  },  {
+    path: 'recuperar',
+    loadChildren: () => import('./recuperar/recuperar.module').then( m => m.RecuperarPageModule)
   },
+
 
 ];
 
