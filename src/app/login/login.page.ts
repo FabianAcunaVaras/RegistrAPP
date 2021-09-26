b1e2da9cd69330e6649d75b3fba9ad6955a5b4fc
Ctapiat1981 <cri.tapiat@duocuc.cl>
2021-09-25 22:21:52 -0300

    boton recuperar

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -30,6 +30,7 @@ export class LoginPage implements OnInit {
         user: this.user
       }
     };
+   
     if(this.validateModel(this.user)){
       this.usuarioServiceS=this.usuarioService.getUsuario(this.user.usuario);
       if(this.usuarioService.getUsuario(this.user.usuario).password === this.user.password){
@@ -45,6 +46,28 @@ export class LoginPage implements OnInit {
     }
 
   }
+
+  recuperar(){
+    const navigationExtras: NavigationExtras = {
+      state: {
+        user: this.user
+      }
+    };
+   
+    if(this.validateUsuario(this.user)){
+      //this.usuarioServiceS=this.usuarioService.getUsuario(this.user.usuario);
+      if(this.usuarioService.getUsuario(this.user.usuario).usuario === this.user.usuario){
+        this.router.navigate(['/principal/', this.user.usuario], navigationExtras);
+      }else{
+        this.presentToast('Nombre de usuario no valido');
+      }
+    }
+    else
+    {
+      this.presentToast('Nombre de Usuario no puede estar Vacio');
+    }
+
+  } 
   async presentToast(message: string, duration?: number){
     const toast = await this.toastController.create(
       {
@@ -64,6 +87,23 @@ export class LoginPage implements OnInit {
         
         return false;
       }
+    }
+    return true;
+  }
+
+  validateUsuario(NombreUsuario: any){
+    
+    // for (const [key, value] of Object.entries(model)) {
+      
+    //   if (value==='') {
+        
+    //     this.campo=key;
+        
+    //     return false;
+    //   }
+    // }
+    if (NombreUsuario===''){
+      return false;
     }
     return true;
   }
